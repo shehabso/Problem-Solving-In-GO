6a035a3d4a94f97754b9001d362105e2e1bcfe39
shehab tarek <58656735+shehabso@users.noreply.github.com>
2025-06-24 12:58:42 +0300

    Time: 0 ms (100%), Space: 4.6 MB (56.25%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.go
@@ -0,0 +1,15 @@
+func maximumWealth(accounts [][]int) int {
+    maxWealth := 0
+    var wealth int
+
+    for _, people := range accounts {
+        wealth = 0
+        for _, bank := range people {
+            wealth += bank
+        }
+        if wealth > maxWealth {
+            maxWealth = wealth
+        }
+    }
+    return maxWealth
+}
